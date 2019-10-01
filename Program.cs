24688cb658e23542a4e31169b3d8b7c491c6225e
revan07 <imperiodecrystal@gmail.com>
2019-10-01 12:57:34 -0400

    name_age.cs

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace nombre_edad
+{
+    class Persona
+    {
+
+        private string nombre;
+        private int edad;
+        public void Inicializar()
+        {
+            Console.Write("Ingrese el nombre:");
+            nombre = Console.ReadLine();
+            string linea;
+            Console.Write("Ingrese la edad:");
+            linea = Console.ReadLine();
+            edad = int.Parse(linea);
+        }
+        public void Imprimir()
+        {
+            Console.Write("Nombre:");
+            Console.WriteLine(nombre);
+            Console.Write("Edad:");
+            Console.WriteLine(edad);
+        }
+        public void EsMayorEdad()
+        {
+            if (edad >= 18)
+            {
+                Console.Write("Es mayor de edad");
+            }
+            else
+            {
+                Console.Write("No es mayor de edad");
+            }
+            Console.ReadKey();
+        }
+    }
+}
